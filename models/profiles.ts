b473f723e52a1ed1499c5da18f5d007b62db0320
IsraelBlack <91918229+IsraelBlack@users.noreply.github.com>
2023-06-29 16:37:49 +1200

    Empty component files added

---
+++ models/profiles.ts
@@ -0,0 +1,17 @@
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// !!These models are for the dumby data ONLY !!
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+export interface Profile {
+  id: number
+  name: string
+  age: number
+  gender: string
+  preference: string
+  bio: string
+  birthday: string
+  image: string
+  star_sign: string
+  matches: string
+  compatibility: string
+}
