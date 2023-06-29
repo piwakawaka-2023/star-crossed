37c6a58eb0c1e794df3a09c20e0b168e6e3cceb2
Israel Black <israelblack98@outlook.com>
2023-06-30 10:03:15 +1200

    Added json db and model

---
+++ client/components/Nav.tsx
@@ -0,0 +1,17 @@
+/****************
+ *    NAVBAR    *
+ ****************/
+
+//imports
+import { slide as Menu} from 'react-burger-menu'
+
+//function
+export default function Nav() {
+  return (
+    <div className="nav-containter">
+        <h2 className="title">Star Crossed</h2>
+
+    </div>
+  )
+}
+
---
+++ models/profile.ts
@@ -0,0 +1,13 @@
+export interface Profile {
+  id: number
+  name: string
+  age: number
+  gender: string
+  preference: string
+  bio: string
+  birthday: string
+  image: string
+  star_Sign: string
+  matches: string
+  compatibility: string
+}
