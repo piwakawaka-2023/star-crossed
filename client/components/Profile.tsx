0c846a6694ba47a79bac4447f4fe5b8ed3bfc7d0
Israel Black <israelblack98@outlook.com>
2023-07-01 12:02:10 +1200

    name and starsign above img

--- client/components/Profile.tsx
+++ client/components/Profile.tsx
@@ -16,22 +16,24 @@ export default function DisplayProfile() {
     const newData = await api.getProfiles()
     setProfiles(newData)
   }
-  
+
   return (
     <div className="profile-container">
       {/* Test button */}
-       {/* <button onClick={handleClick}></button> */}
-      {/* eslint-disable-next-line react/no-unescaped-entities*/}
-      
+      {/* <button onClick={handleClick}></button> */}
+
+      <h1>
+        {/* eslint-disable-next-line react/no-unescaped-entities*/}
+        {testProfile.name}'s Profile {testProfile.starSign}
+      </h1>
+
       <img src={'/' + testProfile.image} alt="Steve's profile" />
-      
-      <h1>{testProfile.name}'s Profile</h1>
       <p className="profile-Info">Age: {testProfile.age}</p>
       <p className="profile-Info">Gender: {testProfile.gender}</p>
       <p className="profile-Info">Prefers: {testProfile.preference}</p>
       <p className="profile-Info">Bio: {testProfile.bio}</p>
       <p className="profile-Info">DoB: {testProfile.birthday}</p>
-      <p className="profile-Info">{testProfile.starSign}</p>
+
       <p className="profile-Info">{testProfile.matches}</p>
       <p className="profile-Info">{testProfile.compatibility}</p>
     </div>
