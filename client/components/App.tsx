15e212bf95ed8bd2be262bf76222cff27d47ac06
Finn Connell <finn.bc@protonmail.com>
2023-07-03 10:24:12 +1200

    removes broken imports

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -4,14 +4,12 @@ import { useAppDispatch } from '../hooks/hooks'
 //Components
 import Header from './Header'
 import DisplayProfile from './Profile'
-import Footer from './Footer'
 
 function App() {
   return (
     <div>
       <Header />
       <DisplayProfile />
-      <Footer />
     </div>
   )
 }
