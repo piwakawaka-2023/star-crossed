a982c6b2f369ef4077e9521962c76bbd6f602ac0
Israel Black <israelblack98@outlook.com>
2023-07-02 12:03:06 +1200

    Placed nav buttons, needs moving

--- client/components/Footer.tsx
+++ client/components/Footer.tsx
@@ -5,12 +5,24 @@
 //function
 export default function Header() {
   return (
-    <div className="footer-container">
-        <h2 className="title">Star Crossed</h2>
-        <div className="logout-container">
-          <a href="/"></a>
-          <button >Logout</button>
-        </div>
-    </div>
+    <nav className="nav-container">
+      <ul>
+        <li>
+          <a href="/" className="nav-item">
+            <img src="./images/icons/profileImg.png" alt="profile" />
+          </a>
+        </li>
+        <li>
+          <a href="/chat" className="nav-item">
+            <img src="./images/icons/chat.png" alt="chat" />
+          </a>
+        </li>
+        <li>
+          <a href="/settings" className="nav-item">
+            <img src="./images/icons/settings.png" alt="settings" />
+          </a>
+        </li>
+      </ul>
+    </nav>
   )
 }
