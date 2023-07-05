97e755eee1888fd456dbc76f64de1310739bd94c
Finn Connell <finn.bc@protonmail.com>
2023-07-06 01:41:35 +1200

    divhell

--- client/components/Messages.tsx
+++ client/components/Messages.tsx
@@ -7,7 +7,7 @@ import { getMatches } from '../apis/users'
 
 function Messages() {
   const [chats, setChats] = useState([] as User[])
-  const profile = useAppSelector((state) => state.profile)
+  const profile = useAppSelector((state) => state.profile) as User
 
   useEffect(() => {
     getChats()
@@ -21,19 +21,24 @@ function Messages() {
   return (
     <div>
       <Header />
-      {chats.map((match: User) => {
-        return (
-          <div key={match.id}>
-            <img src={match.image} alt={match.name} />
-            <h2>{match.name}</h2>
-            <img
-              src={`images/starsigns/${match.star_sign.name}.PNG`}
-              alt={match.star_sign.name}
-            />
-            <p>Send a chat!</p>
-          </div>
-        )
-      })}
+      <div className="chatsContainer">
+        {chats.map((match: User) => {
+          return (
+            <div key={match.id} className="chat">
+              <img className="thumbNail" src={match.image} alt={match.name} />
+              <div className="divHell">
+                <h2>{match.name}</h2>
+                <p>Send a chat!</p>
+              </div>
+              <img
+                className="icon"
+                src={`images/starsigns/${match.star_sign.name}.PNG`}
+                alt={match.star_sign.name}
+              />
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
